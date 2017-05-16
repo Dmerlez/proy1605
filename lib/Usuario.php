8887410b552935613d3f5a74d48795d8ee00c6b8
cetecom <cetecom@LC08L5721>
2017-05-16 10:24:23 -0400

    hola

--- lib/Usuario.php
+++ lib/Usuario.php
@@ -26,6 +26,28 @@ class Usuario{
               
     }
     
+    function VerificaUsuarioClave(){
+        $oConn=new Conexion();
+        
+        if ($oConn->conectar())
+            $db=$oConn->objconn;
+        else
+            return false;
+        
+        $clavemd5=md5($this->clave);
+        $sql="SELECT * FROM acceso WHERE nomusuario='$this->nombre' and pwdusuario='$clavemd5'";
+        
+        echo $sql;
+               
+        $resultado=$db->query($sql);
+        
+        if ($resultado->num_rows>=1)
+            return true;
+        else
+            return false;
+              
+    }
+    
 }
 
 
