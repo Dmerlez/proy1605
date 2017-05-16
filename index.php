5c7da9f71496096edd4acddfc4ac9b3f790b27cc
cetecom <cetecom@LC08L5721>
2017-05-16 10:05:56 -0400

    hola

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+<?php
+/*session_start();
+$_SESSION["saludo"]="Hola mundo";*/
+
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <a href="revision"> Revisi&oacute;n sesi&oacute;n </a>
     </body>
 </html>
